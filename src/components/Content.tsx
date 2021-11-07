aab03e2b650c8610f748f01b92e8a7cfb96438fa
ViniciusEmalheiro <viniciusedu007sk8@gmail.com>
2021-11-07 00:05:12 -0300

    Desafio -Componentizando-Aplicação

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,24 @@
+import { useMovies } from "../MovieContext"
+import { MovieCard } from "./MovieCard"
+
 export function Content() {
-  // Complete aqui
+
+  const { genres, movies, selectedGenre}  = useMovies();
+
+
+  return (
+      <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movies => (
+              <MovieCard key ={movies.Title} title={movies.Title} poster={movies.Poster} runtime={movies.Runtime} rating={movies.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
 }
